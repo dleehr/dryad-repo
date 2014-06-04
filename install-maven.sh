f21559eb2fcab7f00950d20855bad15ee73e56db
Dan Leehr <dan.leehr@nescent.org>
2014-06-04 16:51:17 -0400

    Use maven from bin distribution

---
+++ install-maven.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -ex
+wget http://archive.apache.org/dist/maven/binaries/apache-maven-2.2.1-bin.tar.gz -O /tmp/apache-maven-2.2.1-bin.tar.gz
+tar xzf /tmp/apache-maven-2.2.1-bin.tar.gz
