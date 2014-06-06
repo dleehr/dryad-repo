89cc7c4faf4f10c0ebaaf9d4e61823bea6f36944
Dan Leehr <dan.leehr@nescent.org>
2014-06-06 16:16:34 -0400

    /opt/dryad-test not /opt/dryad

--- test/bin/travis-install-dryad-dir.sh
+++ test/bin/travis-install-dryad-dir.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo mkdir -p -m 0755 /opt/dryad
-sudo chown -R $USER /opt/dryad
-cp -r $TRAVIS_BUILD_DIR/test/config /opt/dryad/config
+sudo mkdir -p -m 0755 /opt/dryad-test
+sudo chown -R $USER /opt/dryad-test
+cp -r $TRAVIS_BUILD_DIR/test/config /opt/dryad-test/config
